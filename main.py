7ee5a7c016eb7c9b40ee36a0ae470481f95efaab
Promodh <promodh2003@gmail.com>
2023-04-21 19:20:31 +0530

    Send an email with news titles and descriptions.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
+from send_email import send_email
 
 api_key = "490151a62b4049ca8a9d1ffa73602ccc"
-url = "https://newsapi.org/v2/everything?q=" \
-        "tesla&from=2023-03-19&sortBy=publishedAt&apiKey=" \
-        "490151a62b4049ca8a9d1ffa73602ccc"
+url = "https://newsapi.org/v2/everything?q="\
+    "tesla&from=2023-03-21&sortBy=publishedAt&apiKey="\
+    "490151a62b4049ca8a9d1ffa73602ccc"
 
 # Make request
 req = requests.get(url)
@@ -11,7 +12,11 @@ req = requests.get(url)
 # Get a dictionary with data
 content = req.json()
 
+body = ""
 # Access the article titles and description
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
---
+++ send_email.py
@@ -0,0 +1,15 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "promodh2003@gmail.com"
+    password = "ljyaiqylvwibwkbs"
+    receiver = "promodh2003@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
